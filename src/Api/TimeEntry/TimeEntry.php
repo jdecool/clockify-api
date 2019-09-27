3db755a8d65d7a63f62d477afdd5b66d1761e09e
Jérémy DECOOL <contact@jdecool.fr>
2019-09-27 21:44:10 +0200

    Add TimeEntry get entry API

--- src/Api/TimeEntry/TimeEntry.php
+++ src/Api/TimeEntry/TimeEntry.php
@@ -6,6 +6,7 @@ namespace JDecool\Clockify\Api\TimeEntry;
 
 use JDecool\Clockify\{
     Client,
+    Exception\ClockifyException,
     Model\TimeEntryDtoImpl
 };
 
@@ -21,6 +22,21 @@ class TimeEntry
     public function create(string $workspaceId, TimeEntryRequest $request): TimeEntryDtoImpl
     {
         $data = $this->http->post(" /workspaces/$workspaceId/time-entries", $request->toArray());
+
+        return TimeEntryDtoImpl::fromArray($data);
+    }
+
+    public function entry(string $workspaceId, string $id, array $params = []): TimeEntryDtoImpl
+    {
+        if (isset($params['consider-duration-format']) && !is_bool($params['consider-duration-format'])) {
+            throw new ClockifyException('Invalid "consider-duration-format" parameter (should be a boolean value)');
+        }
+
+        if (isset($params['hydrated']) && !is_bool($params['hydrated'])) {
+            throw new ClockifyException('Invalid "hydrated" parameter (should be a boolean value)');
+        }
+
+        $data = $this->http->get("/workspaces/$workspaceId/time-entries/$id", $params);
 
         return TimeEntryDtoImpl::fromArray($data);
     }

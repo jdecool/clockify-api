d7251732ef5b96222626f3550e351a652ecb0cbf
Jérémy DECOOL <contact@jdecool.fr>
2020-09-30 21:31:09 +0200

    Use PSR-18 compatible HttpClient

--- src/ClientBuilder.php
+++ src/ClientBuilder.php
@@ -6,14 +6,23 @@ namespace JDecool\Clockify;
 
 use Http\Client\{
     Common\HttpMethodsClient,
+    Common\Plugin\AddHostPlugin,
+    Common\Plugin\AddPathPlugin,
+    Common\Plugin\AuthenticationPlugin,
+    Common\Plugin\HeaderSetPlugin,
+    Common\PluginClient,
     HttpClient,
 };
-use Http\Message\MessageFactory;
-use Psr\Http\Message\RequestFactoryInterface;
+use Http\Message\Authentication\Header;
+use Psr\Http\Message\{
+    RequestFactoryInterface,
+    UriFactoryInterface,
+};
 use Http\Discovery\{
     Psr17FactoryDiscovery,
     Psr18ClientDiscovery,
 };
+use RuntimeException;
 
 class ClientBuilder
 {
@@ -21,11 +30,16 @@ class ClientBuilder
 
     private $httpClient;
     private $requestFactory;
+    private $uriFactory;
 
-    public function __construct(?HttpClient $httpClient = null, ?RequestFactoryInterface $requestFactory = null)
-    {
+    public function __construct(
+        ?HttpClient $httpClient = null,
+        ?RequestFactoryInterface $requestFactory = null,
+        ?UriFactoryInterface $uriFactory = null
+    ) {
         $this->httpClient = $httpClient ?? Psr18ClientDiscovery::find();
         $this->requestFactory = $requestFactory ?? Psr17FactoryDiscovery::findRequestFactory();
+        $this->uriFactory = $uriFactory ?? Psr17FactoryDiscovery::findUriFactory();
     }
 
     public function createClientV1(string $apiKey): Client
@@ -35,8 +49,35 @@ class ClientBuilder
 
     public function create(string $endpoint, string $apiKey): Client
     {
-        $http = new HttpMethodsClient($this->httpClient, $this->requestFactory);
+        if (false === filter_var($endpoint, FILTER_VALIDATE_URL)) {
+            throw new RuntimeException('Invalid Clockify endpoint.');
+        }
 
-        return new Client($http, $endpoint, $apiKey);
+        if ('' === trim($apiKey)) {
+            throw new RuntimeException('API token is required.');
+        }
+
+        $plugins = [
+            new AuthenticationPlugin(
+                new Header('X-Api-Key', $apiKey),
+            ),
+            new AddHostPlugin(
+                $this->uriFactory->createUri($endpoint),
+            ),
+            new AddPathPlugin(
+                $this->uriFactory->createUri($endpoint),
+            ),
+            new HeaderSetPlugin([
+                'User-Agent' => 'github.com/jdecool/clockify-api',
+                'Content-Type' => 'application/json',
+            ]),
+        ];
+
+        $http = new HttpMethodsClient(
+            new PluginClient($this->httpClient, $plugins),
+            $this->requestFactory,
+        );
+
+        return new Client($http);
     }
 }
